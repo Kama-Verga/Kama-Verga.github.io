b40cf17176e2381aae9e6dfb126ed09c1582c916
Kama-Verga <132174425+Kama-Verga@users.noreply.github.com>
2023-07-07 19:07:25 +0200

    va tutto

--- Bomb/script.js
+++ Bomb/script.js
@@ -1,15 +1,14 @@
 var Boom = new Howl({
-  src: ['Boom.mp3']
+  src: ['Boom.ogg']
 });
 
 var Tick = new Howl({
-  src: ['Ticktack.mp3']
+  src: ['Ticktack.ogg']
 });
-
-var Boom = document.getElementById("#Boom")
 
 let list = [];
 let OldList = [];
+let button = document.getElementById("#NextButton");
 
 fetch('Database.csv')
 .then(response => response.text())
@@ -21,18 +20,34 @@ fetch('Database.csv')
   }
 });
 
-function playAudioLoop() {
-  Boom.play();
-  Tick.play();
+function playAudioLoop(n) {
+  if(n > 0){ 
+    Tick.play();
+    setTimeout(function() {
+      playAudioLoop(n - 1); // Richiama la funzione per la prossima iterazione con un ritardo
+    }, 3.1 * 1000);
+  }
 }
 
-function Next(){
-  playAudioLoop();
-  var i;
-  do {
-   i = Math.floor(Math.random() * list.length);    
-  } while (OldList.includes(i) && OldList.length < list.length -1);
-  OldList.push(i);
-  document.querySelector("#text").innerHTML = list[i];
+var bool = true;
 
+function Next(){
+  if(bool){
+    bool = false;
+    var i;
+    do {
+      i = Math.floor(Math.random() * list.length);    
+    } while (OldList.includes(i) && OldList.length < list.length -1);
+    OldList.push(i);
+    document.querySelector("#text").innerHTML = list[i];
+    var n = 1;/* Math.floor(Math.random() * 15)*/
+
+    let Sn = n;
+    playAudioLoop(n);
+
+    setTimeout(function() {
+      Boom.play();
+      bool = true;
+    }, 3.1 * Sn * 1000);
+  }
 }
